c1b1baf0ba5c4c9cdea9e3c0f273edcfd2faeb16
Madheshrmedia <madhesh.rmedia1123@gmail.com>
2024-07-04 17:46:52 +0530

    sliding nav 4 jul by madhesh

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 
+
     <style>
         * {
             font-family: "Inter", sans-serif;
@@ -64,39 +65,6 @@
 </head>
 
 <div class="navbar_div ">
-    <!-- <nav class="navbar navbar-expand sticky-top navbar-background">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="./image/mixture-tvlogo.png" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">about us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">contact us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blogs</a>
-                    </li>
-
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav> -->
 
     <nav class="navbar navbar-expand-lg navbar-background">
         <div class="container-fluid">
--- index.php
+++ index.php
@@ -1,8 +1,114 @@
 <?php
 include_once ("./header.php");
 ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+
+<style>
+    .slide-menu-a {
+        background: red;
+    }
+
+    .slide-menu-a:hover {
+        background: pink;
+    }
+</style>
 
 <body>
+
+    <!-- Set up your HTML -->
+    <div class="owl-carousel">
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending1
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending2
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending3
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending4
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending5
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending6
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending7
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending8
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending9
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending10
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending11
+            </a>
+        </div>
+        <div class="slide-menu">
+            <a href="#" class=" btn  slide-menu-a">
+                #trending12
+            </a>
+        </div>
+    </div>
+
+
+    <?php
+    include_once ("./footerlink.php");
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 3,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 3,
+                    nav: false
+                },
+                1000: {
+                    items: 14,
+                    nav: true,
+                    loop: false
+                }
+            }
+        })
+    </script>
 
 </body>
 
